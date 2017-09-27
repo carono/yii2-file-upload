562bd88fbaf5dbb8b1abe00871b752d6c2e946b0
carono <info@carono.ru>
2017-09-27 22:00:32 +0500

    \carono\yii2file\FileUploadTrait

---
+++ FileUploadTrait.php
@@ -0,0 +1,132 @@
+<?php
+
+
+namespace carono\yii2file;
+
+use yii\db\ActiveRecord;
+use yii\helpers\FileHelper;
+
+/**
+ * Trait FileUploadTrait
+ *
+ * @package carono\yii2file
+ * @property integer $id
+ * @property string $uid
+ * @property integer $user_id
+ * @property string $name
+ * @property string $extension
+ * @property string $folder
+ * @property string $mime_type
+ * @property integer $size
+ * @property string $data
+ * @property string $session
+ * @property string $md5
+ * @property string $slug
+ * @property integer $is_active
+ * @property integer $is_exist
+ * @property resource $binary
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property string $fileName
+ * @property string $realFileName
+ * @property string $realFilePath
+ *
+ * @mixin ActiveRecord
+ */
+trait FileUploadTrait
+{
+    public $fileNameAsUid = false;
+    public $eraseOnDelete = true;
+    public $uploaderClass = 'carono\yii2file\Uploader';
+
+    public function init()
+    {
+        $this->on(self::EVENT_BEFORE_DELETE, [$this, 'eraseOnDelete']);
+        parent::init();
+    }
+
+    /**
+     * @param $file
+     * @return Uploader|mixed
+     */
+    public static function startUpload($file)
+    {
+        $class = (new self())->uploaderClass;
+        return \Yii::createObject([
+            'modelClass' => self::className(),
+            'class' => $class,
+            'file' => $file,
+            'folder' => '@app/files'
+        ]);
+    }
+
+    public function deleteFile()
+    {
+        if ($this->fileExist()) {
+            @unlink($this->getRealFilePath());
+            if ($f = !$this->fileExist()) {
+                self::updateAttributes(["is_exist" => false]);
+            }
+            return $f;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getRealFileName()
+    {
+        if ($this->fileNameAsUid == true) {
+            return $this->uid . '.' . $this->extension;
+        } else {
+            return $this->name . '.' . $this->extension;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRealFilePath()
+    {
+        $path = \Yii::getAlias($this->folder) . DIRECTORY_SEPARATOR . $this->getRealFileName();
+        return str_replace('/', DIRECTORY_SEPARATOR, $path);
+    }
+
+    /**
+     * @return bool
+     */
+    public function fileExist()
+    {
+        return file_exists($this->getRealFilePath());
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function isImage()
+    {
+        if (($mime = $this->mime_type) || ($mime = FileHelper::getMimeType($this->getRealFilePath()))) {
+            return strpos($mime, 'image') === 0;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileName()
+    {
+        return join('.', array_filter([$this->name, $this->extension]));
+    }
+
+    public function eraseOnDelete()
+    {
+        if ($this->eraseOnDelete && $this->fileExist()) {
+            $this->deleteFile();
+        }
+    }
+}
---
+++ Uploader.php
@@ -0,0 +1,284 @@
+<?php
+
+namespace carono\yii2file;
+
+use Yii;
+use yii\base\Component;
+use yii\base\Security;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
+use yii\helpers\ArrayHelper;
+use yii\helpers\FileHelper;
+use yii\web\UploadedFile;
+
+/**
+ * Class FileUpload
+ *
+ * @package carono\components
+ * @property string $fullName
+ */
+class Uploader extends Component
+{
+    /**
+     * @var FileUploadTrait
+     */
+    public $modelClass;
+    public $data;
+    public $file;
+    public $name;
+    public $slug;
+    public $uid;
+    public $delete = true;
+    public $folder;
+
+    protected $fileName;
+    protected $filePath;
+
+    /**
+     * @param $name
+     * @return $this
+     */
+    public function name($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @param $data
+     * @return $this
+     */
+    public function data($data)
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    /**
+     * @param $path
+     * @return $this
+     */
+    public function folder($path)
+    {
+        $this->folder = $path;
+        return $this;
+    }
+
+    /**
+     * @param bool $deleteOnFinish
+     * @return $this
+     */
+    public function delete($deleteOnFinish = true)
+    {
+        $this->delete = $deleteOnFinish;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    protected function getSession()
+    {
+        if (isset(Yii::$app->session)) {
+            Yii::$app->session->open();
+            $session = Yii::$app->session->getIsActive() ? Yii::$app->session->getId() : null;
+            Yii::$app->session->close();
+        } else {
+            $session = null;
+        }
+        return $session;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFileName()
+    {
+        return $this->name ?: $this->fileName;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getFileNameWithOutExtension()
+    {
+        return basename($this->getFileName(), '.' . $this->getFileExtension());
+    }
+
+    /**
+     * @return string
+     */
+    protected function getMimeType()
+    {
+        return FileHelper::getMimeType($this->filePath);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    protected function getFileExtension()
+    {
+        if (!$extension = strtolower(pathinfo($this->fileName, PATHINFO_EXTENSION))) {
+            $extension = ArrayHelper::getValue(FileHelper::getExtensionsByMimeType($this->getMimeType()), 0);
+        }
+        return $extension;
+    }
+
+    /**
+     * @return int|null|string
+     */
+    public function getUserId()
+    {
+        if (isset(Yii::$app->user)) {
+            $userId = Yii::$app->user->getId();
+        } else {
+            $userId = null;
+        }
+        return $userId;
+    }
+
+    /**
+     * @param $source
+     * @param $destination
+     * @throws \Exception
+     */
+    public function copy($source, $destination)
+    {
+        if (is_uploaded_file($source)) {
+            if (!move_uploaded_file($source, $destination)) {
+                throw new \Exception('Unknown upload error');
+            }
+        } elseif ($this->delete ? !rename($source, $destination) : !copy($source, $destination)) {
+            throw new \Exception('Failed to write file to disk');
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getUid()
+    {
+        return $this->uid = $this->uid ?: $this->formUid();
+    }
+
+    /**
+     * @return string
+     */
+    public function getNewFileAlias()
+    {
+        return self::formAliasPath($this->getUid(), $this->folder);
+    }
+
+    /**
+     * @param ActiveRecord $model
+     * @param $attributes
+     */
+    public function loadAttributes($model, $attributes)
+    {
+        $model->setAttributes($attributes);
+    }
+
+    /**
+     * @return FileUploadTrait|null
+     * @throws \Exception
+     */
+    public function process()
+    {
+        /**
+         * @var FileUploadTrait $model
+         */
+        $model = new $this->modelClass();
+        $this->processFilePath($this->file);
+        $this->loadAttributes($model, [
+            'session' => $this->getSession(),
+            'name' => $this->getFileNameWithOutExtension(),
+            'extension' => $this->getFileExtension(),
+            'user_id' => $this->getUserId(),
+            'uid' => $this->getUid(),
+            'data' => !is_null($this->data) ? json_encode($this->data) : null,
+            'mime_type' => $this->getMimeType(),
+            'md5' => md5_file($this->filePath),
+            'folder' => self::formAliasPath($this->getUid(), $this->folder),
+            'slug' => $this->slug,
+            'size' => filesize($this->filePath)
+        ]);
+        $newFilePath = $model->getRealFilePath();
+        if (!is_dir($realFolder = dirname($newFilePath))) {
+            mkdir($realFolder, 0777, true);
+        }
+        if (!file_exists($this->filePath)) {
+            throw new \Exception('File not loaded or not exist');
+        }
+        $this->copy($this->filePath, $newFilePath);
+        if ($model->save()) {
+            return $model;
+        } else {
+            $model->deleteFile();
+            return null;
+        }
+    }
+
+    /**
+     * @param $file
+     */
+    protected function processFilePath($file)
+    {
+        if (strpos($file, 'http') === 0) {
+            $tmp = Yii::getAlias('@runtime') . DIRECTORY_SEPARATOR . uniqid("fu");
+            file_put_contents($tmp, file_get_contents($file));
+            $this->filePath = $tmp;
+            $this->fileName = basename($file);
+        } elseif (is_string($file)) {
+            $this->filePath = Yii::getAlias($file);
+            $this->fileName = basename($this->filePath);
+        } elseif ($file instanceof UploadedFile) {
+            $this->filePath = $file->tempName;
+            $this->fileName = $file->name;
+        }
+    }
+
+    /**
+     * @param $slug
+     * @return $this
+     */
+    public function slug($slug)
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    protected function generateUid()
+    {
+        $sec = new Security();
+        return md5($sec->generateRandomString(64));
+    }
+
+    /**
+     * @return string
+     */
+    protected function formUid()
+    {
+        $class = $this->modelClass;
+        do {
+            $uPath = $this->generateUid();
+        } while ($class::find()->where(["uid" => $uPath])->exists());
+        return $uPath;
+    }
+
+    /**
+     * @param $path
+     * @param null $aliasPrefix
+     * @return string
+     */
+    public static function formAliasPath($path, $aliasPrefix = null)
+    {
+        $p = [$aliasPrefix];
+        for ($i = 0; $i < 3; $i++) {
+            $p[] = $path[$i];
+        }
+        return join('/', $p);
+    }
+}
