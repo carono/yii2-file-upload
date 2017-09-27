185a1d752803580f496c49683b692b84f6d7de12
carono <info@carono.ru>
2017-09-27 22:00:13 +0500

    add trait \carono\yii2file\FileUploadTrait

--- migrations/m161228_100819_init.php
+++ migrations/m161228_100819_init.php
@@ -1,38 +1,41 @@
 <?php
 
-class m161228_100819_init extends \carono\yii2installer\Migration
+class m161228_100819_init extends \yii\db\Migration
 {
+    use \carono\yii2migrate\traits\MigrationTrait;
+
+    public $tableName = '{{%file_upload}}';
+
     public function newTables()
     {
-        $tableName = 'file_upload';
-        $table = [
-            $tableName => [
-                'id'         => self::primaryKey(),
-                'uid'        => self::string(64)->unique(),
-                'user_id'    => self::integer(),
-                'name'       => self::string(),
-                'extension'  => self::string(),
-                'folder'     => self::string(),
-                'mime_type'  => self::string(),
-                'size'       => self::integer(),
-                'data'       => self::text(),
-                'session'    => self::string(),
-                'md5'        => self::string(32),
-                'slug'       => self::string(),
-                'is_active'  => self::boolean()->notNull()->defaultValue(true),
-                'is_exist'   => self::boolean()->notNull()->defaultValue(true),
-                'binary'     => self::binary(),
+        return [
+            $this->tableName => [
+                'id' => self::primaryKey(),
+                'uid' => self::string(32)->unique(),
+                'user_id' => self::integer(),
+                'name' => self::string(),
+                'extension' => self::string(),
+                'folder' => self::string(),
+                'mime_type' => self::string(),
+                'size' => self::integer(),
+                'data' => self::text(),
+                'session' => self::string(),
+                'md5' => self::string(32),
+                'slug' => self::string(),
+                'is_active' => self::boolean()->notNull()->defaultValue(true),
+                'is_exist' => self::boolean()->notNull()->defaultValue(true),
+                'binary' => self::binary(),
                 'created_at' => self::dateTime(),
                 'updated_at' => self::dateTime()
             ]
         ];
-        return $table;
     }
 
     public function newIndex()
     {
         return [
-            ['file_upload', ['is_active', 'is_exist']]
+            ['file_upload', ['is_active', 'is_exist']],
+            ['file_upload', 'slug'],
         ];
     }
 
