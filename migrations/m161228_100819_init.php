9e741354f163bd6d5537c55f6f3876c3997733f2
carono <info@carono.ru>
2018-02-28 18:54:18 +0500

    scrutinizer

--- migrations/m161228_100819_init.php
+++ migrations/m161228_100819_init.php
@@ -10,23 +10,23 @@ class m161228_100819_init extends \yii\db\Migration
     {
         return [
             $this->tableName => [
-                'id' => self::primaryKey(),
-                'uid' => self::string(32)->unique()->comment('Уникальный идентификатор, используется для содания директорий хранения'),
-                'user_id' => self::integer()->comment('Текущий пользователь, который сохраняет файл'),
-                'name' => self::string()->comment('Имя файла, без расширения'),
-                'extension' => self::string()->comment('Расширение'),
-                'folder' => self::string()->comment('Папка, где хранится файл, можно использовать @алиасы'),
-                'mime_type' => self::string()->comment('Mime Type по содержимому файла'),
-                'size' => self::integer()->comment('Размер файла'),
-                'data' => self::text()->comment('Произвольные данные'),
-                'session' => self::string()->comment('Сессия текущего пользователя'),
-                'md5' => self::string(32)->comment('MD5 по содержимому файла'),
-                'slug' => self::string()->comment('Произвольный слаг'),
-                'is_active' => self::boolean()->notNull()->defaultValue(true),
-                'is_exist' => self::boolean()->notNull()->defaultValue(true)->comment('Существование реального файла'),
-                'binary' => self::binary()->comment('Файл хранится в базе (на данный момент не используется)'),
-                'created_at' => self::dateTime()->comment('Дата создания'),
-                'updated_at' => self::dateTime()->comment('Дата обновления')
+                'id' => $this->primaryKey(),
+                'uid' => $this->string(32)->unique()->comment('Уникальный идентификатор, используется для содания директорий хранения'),
+                'user_id' => $this->integer()->comment('Текущий пользователь, который сохраняет файл'),
+                'name' => $this->string()->comment('Имя файла, без расширения'),
+                'extension' => $this->string()->comment('Расширение'),
+                'folder' => $this->string()->comment('Папка, где хранится файл, можно использовать @алиасы'),
+                'mime_type' => $this->string()->comment('Mime Type по содержимому файла'),
+                'size' => $this->integer()->comment('Размер файла'),
+                'data' => $this->text()->comment('Произвольные данные'),
+                'session' => $this->string()->comment('Сессия текущего пользователя'),
+                'md5' => $this->string(32)->comment('MD5 по содержимому файла'),
+                'slug' => $this->string()->comment('Произвольный слаг'),
+                'is_active' => $this->boolean()->notNull()->defaultValue(true),
+                'is_exist' => $this->boolean()->notNull()->defaultValue(true)->comment('Существование реального файла'),
+                'binary' => $this->binary()->comment('Файл хранится в базе (на данный момент не используется)'),
+                'created_at' => $this->dateTime()->comment('Дата создания'),
+                'updated_at' => $this->dateTime()->comment('Дата обновления')
             ]
         ];
     }
