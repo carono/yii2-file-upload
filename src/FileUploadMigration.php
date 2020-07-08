9505e971ef00762178d873cd4322cda247c5bd85
carono <info@carono.ru>
2020-07-08 08:37:09 +0500

    Поправлена миграция FileUploadMigration

--- src/FileUploadMigration.php
+++ src/FileUploadMigration.php
@@ -2,7 +2,7 @@
 
 namespace carono\yii2file;
 
-require_once __DIR__ . '/migrations/m161228_100819_init.php';
+require_once dirname(__DIR__) . '/migrations/m161228_100819_init.php';
 
 class FileUploadMigration extends \m161228_100819_init
 {
